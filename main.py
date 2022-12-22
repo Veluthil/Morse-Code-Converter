74e94b13ce2768cb1a8bd15d541ae8ea71776ff4
Martyna Boradyn <martyna.boradyn@gmail.com>
2022-12-22 19:26:10 +0100

    Add turn_into_string function, add "Goodbye" ASCII art.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from art import logo
+from art import logo, goodbye
 
 morse_code = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
               'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
@@ -8,7 +8,7 @@ morse_code = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
               'Z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
               '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
               '0': '-----', ',': '--..--', '.': '.-.-.-', '?': '..--..', '/': '-..-.',
-              '-': '-....-', '(': '-.--.', ')': '-.--.-', "'": '.____.', ' ': '|'
+              '-': '-....-', '(': '-.--.', ')': '-.--.-', "'": '.____.', ' ': ''
               }
 
 
@@ -21,21 +21,59 @@ def turn_into_morse_code(string):
                 converted_string.append(morse_code[key])
 
 
-print(f"{logo}"
-      f"\nWelcome to the Morse Code Converter!"
-      "\n- '|' indicates spaces between words"
-      "\n- To exit Morse Code Converter type '#'")
+def turn_into_string(string):
+    string_list = string.split(' ')
+    for symbols in string_list:
+        if symbols not in morse_code.values():
+            return "Invalid character, try again."
+        for value in morse_code.values():
+            key_list = list(morse_code.keys())
+            val_list = list(morse_code.values())
+            if symbols == value:
+                position = val_list.index(value)
+                converted_string.append(key_list[position])
 
+
+# ------------ INTRODUCTION AND RULES --------------
+print(f"{logo}"
+      "\nWelcome to the Morse Code Converter!"
+      "\n"
+      "\nFirst choose a mode: "
+      "\n- type 'M' if you want to convert Morse Code to text,"
+      "\n- type 'T' if you want to convert text to Morse Code."
+      "\nRules and additional options:"
+      "\n- '  ' (double space) indicates spaces between words in Morse Code -"
+      " when converting Morse Code to a string use double space as a break between single words."
+      "\n- To exit Morse Code Converter type '#'"
+      "\n")
+
+
+# -------------- FUNCTIONALITY -----------------------
 converter_on = True
 converted_string = []
 while converter_on:
-    string_to_convert = input("Please write here a text you would like to convert to Morse Code: ").upper()
-    turn_into_morse_code(string_to_convert)
-    if len(converted_string) != 0:
-        print(f"Your string ('{string_to_convert}') in morse code is: {' '.join(converted_string)}")
-    elif string_to_convert[0] == "#":
+    mode = input("Choose a mode by typing 'M' or 'T': ").upper()
+    if mode == "T":
+        string_to_convert = input("Please write here your text you would like to convert to Morse Code: ").upper()
+        turn_into_morse_code(string_to_convert)
+        if len(converted_string) != 0:
+            print(f"Your text ({string_to_convert}) in Morse Code is: {' '.join(converted_string)}")
+        elif string_to_convert[0] == "#":
+            converter_on = False
+            print(goodbye)
+        else:
+            print("Invalid character, try again.")
+    elif mode == "M":
+        mc_to_convert = input("Please write here Morse Code you would like to convert to text: ")
+        turn_into_string(mc_to_convert)
+        if len(converted_string) != 0:
+            print(f"Your Morse Code ({mc_to_convert}) converted to text is: {''.join(converted_string)}")
+        elif mc_to_convert[0] == "#":
+            converter_on = False
+            print(goodbye)
+        else:
+            print("Invalid character, try again.")
+    elif mode == "#":
         converter_on = False
-        print("Goodbye!")
-    else:
-        print("Invalid character, try again.")
+        print(goodbye)
     converted_string.clear()
